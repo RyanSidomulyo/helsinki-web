018431bb559ff352c98e2cb50d20b9c7bb39ffaf
JoanBokko <sidomulyo784@gmail.com>
2022-11-30 10:12:59 +0800

    menambahkan file helsinki-web

---
+++ helsinki-web/src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © HELSINKI all rights reserved. Powered by HELSNIKI FROM FRONT END CLASS.</p>
+      </footer>
+    </>
+  )
+}
